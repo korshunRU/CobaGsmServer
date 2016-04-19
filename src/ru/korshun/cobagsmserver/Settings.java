1b1f7d1a034509f3afdc45cda68040665ee033e5
korshunRU <kanibal_2002@mail.ru>
2016-04-19 18:43:06 +0500

    1

---
+++ src/ru/korshun/cobagsmserver/Settings.java
@@ -0,0 +1,89 @@
+package ru.korshun.cobagsmserver;
+
+
+@SuppressWarnings("FieldCanBeLocal")
+class Settings {
+
+
+    /**
+     *   Данные для доступа к БД
+     */
+    private final String                HOST_SUFFIX =       "jdbc:mysql://";
+    private final String                HOST =              "localhost:3306/";
+    private final String                DATABASE_NAME =     "coba_web_room";
+    private final String                USERNAME =          "coba";
+    private final String                PASSWORD =          "coba";
+//    private final String                PASSWORD =          ",fhvfktq82";
+    private final String                DB_TABLE_PREFIX =   "coba_";
+
+
+    /**
+     *  Порт, который "слушает" TCP сервер
+     */
+    private final int                   TCP_PORT =          7777;
+
+
+    /**
+     *  Порт, который "слушает" UPD сервер
+     */
+    private final int                   UDP_PORT =          8888;
+
+
+    /**
+     *  Максимальное число одновременных коннектов по всем протоколам
+     */
+    private final int                   MAX_CONNECT_COUNT = 15;
+
+
+    /**
+     *  "Соль" пароля
+     */
+    private final String                PASS_SALT =         "7205c1640724561a622e2866e6f2329b";
+
+
+    Settings() {
+
+
+
+    }
+
+    String getDATABASE_NAME() {
+        return DATABASE_NAME;
+    }
+
+    String getDB_TABLE_PREFIX() {
+        return DB_TABLE_PREFIX;
+    }
+
+    String getHOST() {
+        return HOST;
+    }
+
+    String getHOST_SUFFIX() {
+        return HOST_SUFFIX;
+    }
+
+    String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    String getUSERNAME() {
+        return USERNAME;
+    }
+
+    int getTCP_PORT() {
+        return TCP_PORT;
+    }
+
+    public int getUDP_PORT() {
+        return UDP_PORT;
+    }
+
+    public int getMAX_CONNECT_COUNT() {
+        return MAX_CONNECT_COUNT;
+    }
+
+    public String getPASS_SALT() {
+        return PASS_SALT;
+    }
+}
